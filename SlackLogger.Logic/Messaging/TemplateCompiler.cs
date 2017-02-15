405aad01c7d09fef5607b0d545c2f0fbdc6627c9
Daniel Moeller <djm@scandinavianapps.com>
2017-02-15 07:59:18 +0100

    Improved Json Escaping

--- SlackLogger.Logic/Messaging/TemplateCompiler.cs
+++ SlackLogger.Logic/Messaging/TemplateCompiler.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Reflection;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using System.Web;
 
 namespace SlackLogger.Logic {
     public static class TemplateCompiler {
         private static readonly Regex _statementRegex = new Regex(@"<%\s*(?<statement>.+?)(:(?<format>.+?))?\s*%>", RegexOptions.Compiled);
+
         public static string Compile(MessageTemplate template, object datasource) {
             return _statementRegex.Replace(template.Content, m => {
                 string statement = m.Groups["statement"]?.Value;
@@ -18,7 +16,7 @@ namespace SlackLogger.Logic {
         private static string ReplaceStatement(string statement, string format, object datasource) {
             object result = ReflectionHelper.GetPropertyValue(statement, datasource);
             if (result != null) {
-                return EscapeValue(FormatValue(result, format));
+                return JsonEscapeValue(FormatValue(result, format));
             }
             return $"{{{{ Statement '{statement}' not found }}}}";
         }
@@ -30,8 +28,8 @@ namespace SlackLogger.Logic {
             return string.Format($"{{0:{format}}}", value);
         }
 
-        private static string EscapeValue(string value) {
-            return value.Replace("\\", "\\\\").Replace("\"", "\\\"").Replace("\r\n", "\\n").Replace("\n", "\\n");
+        private static string JsonEscapeValue(string value) {
+            return HttpUtility.JavaScriptStringEncode(value, false);
         }
     }
 }
